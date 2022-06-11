e7c713292d123bc29d18316f5de75d4c38a18c3f
Derrek <a0926884115@gmail.com>
2022-06-11 19:04:05 +0800

    use strict

--- WebRoot/resource/js/main.js
+++ WebRoot/resource/js/main.js
@@ -1,40 +1,41 @@
-$(document).ready(function(){
-    "use strict";
+$(document).ready(function() {
+	"use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('fast');
-        } else {
-            $('.back-to-top').fadeOut('fast');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 0, 'easeInOutExpo');
-        return false;
-    });
+	// Spinner
+	var spinner = function() {
+		setTimeout(function() {
+			if ($('#spinner').length > 0) {
+				$('#spinner').removeClass('show');
+			}
+		}, 1);
+	};
+	spinner();
 
 
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
+	// Back to top button
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 300) {
+			$('.back-to-top').fadeIn('fast');
+		} else {
+			$('.back-to-top').fadeOut('fast');
+		}
+	});
+	
+	$('.back-to-top').on("click", function() {
+		$('html, body', document).animate({ scrollTop: 0 }, 0, 'easeInOutExpo');
+		return false;
+	});
 
 
-    // Chart Global Color
-    Chart.defaults.color = "#6C7293";
-    Chart.defaults.borderColor = "#000000";
-    
+	// Sidebar Toggler
+	$('.sidebar-toggler').click(function() {
+		$('.sidebar, .content').toggleClass("open");
+		return false;
+	});
+
+
+	// Chart Global Color
+	Chart.defaults.color = "#6C7293";
+	Chart.defaults.borderColor = "#000000";
+
 });

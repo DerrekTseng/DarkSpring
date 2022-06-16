0368ad9f972aa30c5ec739cd7d0eb9eec0ff6e0c
Derrek Tseng <a0926884115@gmail.com>
2022-06-16 09:56:34 +0800

    更新回傳物件

--- src/net/derrek/spring/component/ResponseData.java
+++ src/net/derrek/spring/component/ResponseData.java
@@ -4,6 +4,18 @@ public class ResponseData<T> extends ResponseBase {
 
 	T data;
 
+	public static <E> ResponseData<E> create(E data) {
+		ResponseData<E> responseData = new ResponseData<>();
+		responseData.setData(data);
+		return responseData;
+	}
+	
+	public static ResponseData<?> error(Throwable throwable) {
+		ResponseData<?> responseData = new ResponseData<>();
+		responseData.setError(throwable);
+		return responseData;
+	}
+
 	public T getData() {
 		return data;
 	}
